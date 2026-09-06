ea8039b2f565d0c8af54adba5f78d98352cd620e
gp938 <patilsg9182@gmail.com>
2026-09-06 18:03:54 +0530

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('Google has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  console.log('URL:', page.url());
+  console.log('TITLE:', await page.title());
+
+  await expect(page).toHaveTitle(/Playwright/, { timeout: 50000 });
+});
