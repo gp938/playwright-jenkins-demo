55eee28725c762d4aa9e7e16ca3ab8a94caa1796
gp938 <patilsg9182@gmail.com>
2026-09-09 16:52:13 +0530

    Add Allure report5

--- playwright.config.js
+++ playwright.config.js
@@ -31,14 +31,14 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
             ['list'],
-            ['allure-playwright',{
+            ['allure-playwright',{ 
               resultsDir:'allure-results'}]
             ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.google.com',
-    headless:true,
+    headless:false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
