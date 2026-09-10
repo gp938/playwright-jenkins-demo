5838f02865047f4646665235c34be9efcf75c057
gp938 <patilsg9182@gmail.com>
2026-09-10 18:06:27 +0530

    Add login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,3 +5,23 @@ await
 page.goto('https://example.com');
 await expect(page).toHaveTitle(/Example/);
 });
+
+// @ts-check
+//import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://google.com',{ waitUntil: 'domcontentloaded' });
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/google/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/',{ waitUntil: 'domcontentloaded' });
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
