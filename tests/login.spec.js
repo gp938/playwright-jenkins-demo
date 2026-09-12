f03160db6ffd6ef7620a57aaa8403dfbf641841d
gp938 <patilsg9182@gmail.com>
2026-09-12 08:58:23 +0530

    updated login file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,7 +12,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://google.com',{ waitUntil: 'domcontentloaded' });
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Google/);
+  await expect(page).toHaveTitle(/google/);
 });
 
 test('get started link', async ({ page }) => {
