9a514d643b7be15f9a34de530e89b26c066a8a04
gp938 <patilsg9182@gmail.com>
2026-09-12 08:56:35 +0530

    updated playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -39,6 +39,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.google.com',
     headless:false,
+    screenshot: 'only-on-failure',
+    video:'retain-on-failure',
+    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -50,12 +53,12 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'],headless: false },
     },
 
-    /*{
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+   /* {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },*/
