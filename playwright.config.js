e9ac5d4eeef1830e7e74116885cf45c35487479b
gp938 <patilsg9182@gmail.com>
2026-09-17 18:54:52 +0530

    updated new screenshot

--- playwright.config.js
+++ playwright.config.js
@@ -39,6 +39,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.google.com',
     headless:false,
+    screenshot:'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
