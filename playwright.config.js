1eb36d9727b0c13d8274944870bfe01c843be3da
gp938 <patilsg9182@gmail.com>
2026-09-17 15:39:36 +0530

    updated playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -13,31 +13,22 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
- 
- 
   testDir: './tests',
   /* Run tests in files in parallel */
- // fullyParallel: true,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
- // forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 0 ,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  /* workers: process.env.CI ? 1 : undefined, */
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-            ['list'],
-            ['allure-playwright',{ 
-              resultsDir:'allure-results'}]
-            ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://www.google.com',
-    headless:false,
-    screenshot: 'on',
-   // video:'retain-on-failure',
-    
+    // baseURL: 'http://localhost:3000',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -46,19 +37,19 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],headless: false },
+      use: { ...devices['Desktop Chrome'] },
     },
 
    /* {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-   */
-   /* {
+
+    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },*/
-
+    },
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
