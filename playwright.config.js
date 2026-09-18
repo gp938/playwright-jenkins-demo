29eedeb8b90cec82a142fd14679b460b571245f8
gp938 <patilsg9182@gmail.com>
2026-09-18 20:35:50 +0530

    latest changes new

--- playwright.config.js
+++ playwright.config.js
@@ -18,14 +18,14 @@ export default defineConfig({
     ['allure-playwright']
   ],*/
   timeout: 120000,
-  workers: 2,
+  workers: 1,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   /* workers: process.env.CI ? 1 : undefined, */
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
