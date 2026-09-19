56ce97425ca16c7cdb59f159d12b07fb5c855277
gp938 <patilsg9182@gmail.com>
2026-09-19 16:25:12 +0530

    latest changes new jenkins2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,6 +10,7 @@ import { test, expect } from '@playwright/test';
 
   await expect(page).toHaveTitle(/Playwright/, { timeout: 50000 });
 }); */
+ 
   test('Example has title', async ({ page }) => {
   await page.goto('https://example.com', {
     waitUntil: 'domcontentloaded',
@@ -18,7 +19,7 @@ import { test, expect } from '@playwright/test';
   console.log('URL:', page.url());
   console.log('TITLE:', await page.title());
 
-  await expect(page).toHaveTitle(/xampleeeee/, { timeout: 50000 });
+  await expect(page).toHaveTitle(/Example/, { timeout: 50000 });
 });
   test('click test', async ({ page }) => {
   await page.goto('https://example.com');
