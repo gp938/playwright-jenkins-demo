5986bb2b3278cc27178ce0a1af9ea56028440543
gp938 <patilsg9182@gmail.com>
2026-09-20 10:43:43 +0530

    login added

---
+++ tests/login.spec.js
@@ -0,0 +1,36 @@
+//const {test,expect} = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+test('Take screenshot',async({page}) =>{
+  await
+  page.goto("https://www.google.com");
+  await expect(page).toHaveTitle(/Example/);
+  await page.screenshot({
+   // path:'screenshots/google.png',
+    fullpage:true
+
+  });
+});
+test('login test',async ({page}) =>{
+await 
+page.goto('https://example.com');
+await expect(page).toHaveTitle(/Example/);
+});
+// @ts-check
+//import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://google.com',{ waitUntil: 'domcontentloaded' });
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Google/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/',{ waitUntil: 'domcontentloaded' });
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
